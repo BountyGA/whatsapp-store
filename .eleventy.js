acf729483ff16605d46ddbe7805ee7a471f983b4
BountyGA <gabrielbounty19@gmail.com>
2025-09-20 16:36:29 +0100

    Decap page not loading on login4

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,8 @@ module.exports = function(eleventyConfig) {
   // Copy images
   eleventyConfig.addPassthroughCopy("src/images");
 
-  // Copy admin (Decap CMS) folder
-  eleventyConfig.addPassthroughCopy("admin");
+  // Copy admin (Decap CMS) folder from inside src/
+  eleventyConfig.addPassthroughCopy("src/admin");
 
   return {
     dir: { input: "src", output: "_site" }
